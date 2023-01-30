2b820eb45a6655cf55bad0bd1b46eee320502183
anna <anna@sayf.su>
2023-01-30 11:58:33 +0300

    создан класс Car и конструктор

---
+++ src/Car.java
@@ -0,0 +1,26 @@
+/**Создайте класс «Автомобиль», у которого есть:
+ марка (brand),
+ модель (model),
+ объем двигателя в литрах (engineVolume),
+ цвет кузова (color),
+ год производства (year),
+ страна сборки (country).
+ */
+public class Car {
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+    private int year;
+    private String country;
+
+    public Car(String brand, String model, double engineVolume, String color, int year, String country){
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
+}
