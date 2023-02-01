cb072277a327959947fbb0ccce92b3675593bc60
anna <anna@sayf.su>
2023-02-01 14:02:16 +0300

    задание 1. Добавены параметры, изменен конструктор

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import transport.Car;
+
 public class Main {
     /**
      * Создайте объекты для следующих автомобилей:
@@ -9,22 +11,24 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        Car car1 = new Car("Lada", "Granta", 1.7, "желтый", 2015, "Россия");
-        Car car2 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
-        Car car3 = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
-        Car car4 = new Car("KIA", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
-        Car car5 = new Car("Hyundai", "Avante", 1.6, " оранжевый", 2016, "Южная Корея");
-        System.out.println(car1);
-        System.out.println(car2);
-        System.out.println(car3);
-        System.out.println(car4);
-        System.out.println(car5);
+//        Car car1 = new Car("Lada", "Granta", 1.7, "желтый", 2015, "Россия");
+//        Car car2 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
+//        Car car3 = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
+//        Car car4 = new Car("KIA", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
+//        Car car5 = new Car("Hyundai", "Avante", 1.6, " оранжевый", 2016, "Южная Корея");
+//        System.out.println(car1);
+//        System.out.println(car2);
+//        System.out.println(car3);
+//        System.out.println(car4);
+//        System.out.println(car5);
+//
+//        System.out.println();
+//        Car car6 = new Car(null, null, 0, null, 0, null);
+//        System.out.println(car6);
 
-        System.out.println();
-        Car car6 = new Car(null, null, 0, null, 0, null);
-        System.out.println(car6);
+        Car car7 = new Car("BMW", "5", 3.0, "красный", 2022, "Германия",
+                "автомат", "седан", null, 5, false) ;
 
-
-
+        System.out.println(car7);
     }
 }
